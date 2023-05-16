357d7f98748fc0613676349ba08cfd5d49d53d73
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-05-16 11:52:01 -0300

    ejercicio 01 completado

---
+++ src/components/Saludo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Saludo = () => {
+    return (
+        <section className='text-center mt-3'>
+                <h1 className='text-light'>¡Hello World!</h1>
+        </section>
+    );
+};
+
+export default Saludo;
